12524ef2bbcffb989e8741c6b2b45e9ca4d9e698
Matteo Di Noia <dmatteo002@gmail.com>
2023-12-31 13:21:08 +0100

    Changing database ddl

--- tweb-project-old/ddl_script.sql
+++ tweb-project-old/ddl_script.sql
@@ -1,7 +1,14 @@
+DROP TABLE users, friends;
 
-CREATE TABLE "user"(
-    username varchar(20),
-    password varchar(64),
-    issuperuser bool
+CREATE TABLE users(
+    username varchar(20) primary key,
+    password varchar(64) NOT NULL,
+    issuperuser bool NOT NULL
+);
+
+CREATE TABLE friends(
+    username varchar(20) references users(username),
+    friend varchar(20) references users(username),
+    primary key (username, friend)
 );
 
