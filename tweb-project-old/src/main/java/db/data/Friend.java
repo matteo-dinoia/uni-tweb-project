f8dc1423ec52f52111aa46c74d9ab71a89fb4c86
Matteo Di Noia <dmatteo002@gmail.com>
2024-01-03 10:25:28 +0100

    Adding friend crud

--- tweb-project-old/src/main/java/db/data/Friend.java
+++ tweb-project-old/src/main/java/db/data/Friend.java
@@ -1,8 +1,6 @@
 package db.data;
 
 import db.ManagerDB;
-import json.errors.FatalError;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,9 +10,12 @@ import java.util.List;
 
 public class Friend extends ManagerDB {
     private final String username;
+    private final String friend;
 
-    public Friend(String username){ this.username = username; }
-    public String getUsername() { return username; }
+    public Friend(String username, String friend){
+        this.username = username;
+        this.friend = friend;
+    }
 
     public static List<Friend> getFriendsOf(String username) {
         try(Connection conn = getConn()){
@@ -24,13 +25,37 @@ public class Friend extends ManagerDB {
             ResultSet resultSet = ps.executeQuery();
             List<Friend> result = new ArrayList<>();
             while(resultSet.next()){
-                Friend tmp = new Friend(resultSet.getString(0));
+                Friend tmp = new Friend(username, resultSet.getString(1));
                 result.add(tmp);
             }
 
             return result;
-        }catch (SQLException ignored){ throw sqlError(); }
+        }catch (SQLException sqlException){ throw sqlError(sqlException.getMessage()); }
     }
 
+    public boolean addFriendship(){
+        try(Connection conn = getConn()){
+            PreparedStatement ps = conn.prepareStatement(
+                    "INSERT INTO friends (username, friend) VALUES (?,?), (?,?)");
+            ps.setString(1, username);
+            ps.setString(2, friend);
+            ps.setString(3, friend);
+            ps.setString(4, username);
 
+            return ps.executeUpdate() == 2;
+        }catch (SQLException sqlException){ throw sqlError(sqlException.getMessage()); }
+    }
+
+    public boolean removeFriendship(){
+        try(Connection conn = getConn()){
+            PreparedStatement ps = conn.prepareStatement(
+                    "DELETE FROM friends where (username = ? and friend = ?) or (username = ? and friend = ?)");
+            ps.setString(1, username);
+            ps.setString(2, friend);
+            ps.setString(3, friend);
+            ps.setString(4, username);
+
+            return ps.executeUpdate() == 2;
+        }catch (SQLException sqlException){ throw sqlError(sqlException.getMessage()); }
+    }
 }
--- tweb-project-old/src/main/java/servlets/data/FriendsServlet.java
+++ tweb-project-old/src/main/java/servlets/data/FriendsServlet.java
@@ -2,9 +2,12 @@ package servlets.data;
 
 import db.data.Friend;
 import db.data.Login;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
+import json.errors.LoggableError;
 import servlets.BasicServlet;
 
 @WebServlet(name = "friends", value = BasicServlet.FRIENDS_PATH)
@@ -13,5 +16,31 @@ public class FriendsServlet extends BasicServlet<List<Friend>, Friend, Void, Fri
     @Override public List<Friend> doGet(HttpServletRequest request) {
         String username = Login.getCurrentLogin(request.getSession());
         return Friend.getFriendsOf(username);
+    }
+
+    @Override public Friend doPost(HttpServletRequest request) throws IOException{ //TODO MAYBE GSON INPUT
+        String username = Login.getCurrentLogin(request.getSession());
+        String friendName = new Scanner(request.getReader()).next();
+
+        if(username.equals(friendName))
+            throw new LoggableError("Cannot make friends with same user");
+
+        Friend friend = new Friend(username, friendName);
+        if(!friend.addFriendship())
+            throw new LoggableError("Coudn't add friendship (maybe user doesn't exist or friendship already exist)");
+        return friend;
+    }
+
+    @Override public Friend doDelete(HttpServletRequest request) throws IOException{ //TODO MAYBE GSON INPUT
+        String username = Login.getCurrentLogin(request.getSession());
+        String friendName = new Scanner(request.getReader()).next();
+
+        if(username.equals(friendName))
+            throw new LoggableError("Cannot remove friendship with same user");
+
+        Friend friend = new Friend(username, friendName);
+        if(!friend.removeFriendship())
+            throw new LoggableError("Coudn't remove friendship (friendship doesn't exist)");
+        return friend;
     }
 }
