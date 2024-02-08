7cff4664444e5166c78b8d6acddece622c966c7e
Matteo Di Noia <dmatteo002@gmail.com>
2024-02-08 15:37:40 +0100

    Refactoring CORS

--- tweb-project-old/src/main/java/servlets/CorsFilter.java
+++ tweb-project-old/src/main/java/servlets/CorsFilter.java
@@ -13,24 +13,21 @@ public class CorsFilter extends HttpFilter {
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
-        // verifica dell'origine
+        // Adding headers for CORS
         String origin = request.getHeader("Origin");
-        //if (!isValid(origin)) chain.doFilter(request, response);
-
-        // Aggiunta degli header per il CORS
-        response.addHeader("Access-Control-Allow-Credentials", "true");
         response.addHeader("Access-Control-Allow-Origin", origin);
+        response.addHeader("Access-Control-Allow-Credentials", "true");
         response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-        response.addHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, " +
-                "PATCH, HEAD, OPTIONS");
+        response.addHeader("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS");
 
-        // Necessario per il CORS handshake
+        // Needed for CORS handshake
         if (request.getMethod().equals("OPTIONS")) {
             response.setStatus(HttpServletResponse.SC_ACCEPTED);
             return;
         }
 
-        // prosegue la catena di gestione della request
+        // continue doing other filters
         chain.doFilter(request, response);
     }
 }
